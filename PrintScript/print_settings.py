98976166cd702430deb7adc4cd288743f7214cbf
mickdupreez <50512885+mickdupreez@users.noreply.github.com>
2025-03-19 16:17:43 +1100

    Create print_settings.py

---
+++ PrintScript/print_settings.py
@@ -0,0 +1,6 @@
+{
+    "target_folder": "~/Downloads",
+    "keywords": ["WorkConfirmation", "label_", "_shipper", "PackingList"],
+    "use_default_printer": true,
+    "explicit_printer_name": "ServicePrinter"
+}
